721c956d8ff309cdd90f5b466250553f2e179d9f
Ivo C.S. Wingelaar <ivo@codeforge.rs>
2021-03-12 18:01:37 +0100

    (objdump): print basic header information

--- objdump/src/main.rs
+++ objdump/src/main.rs
@@ -15,18 +15,57 @@ fn create_clap_app() -> App<'static, 'static> {
 
 use anyhow::{Context, Result};
 use mmb_parser::Mmb;
+use std::path::Path;
 
 fn main() -> Result<()> {
     let app = create_clap_app();
     let matches = app.get_matches();
 
     let path = matches.value_of("PATH").expect("required argument");
-    let data = std::fs::read(path)
-        .with_context(|| format!("Could not read binary proof file from \"{}\"", path))?;
+    let path = Path::new(path);
+    let data = std::fs::read(path).with_context(|| {
+        format!(
+            "Could not read binary proof file from \"{}\"",
+            path.display()
+        )
+    })?;
 
     let mmb = Mmb::from(&data).unwrap();
 
-    println!("{}", mmb.version);
+    handle(path, mmb)?;
+
+    Ok(())
+}
+
+fn print_offsets(mmb: &Mmb, range: &[u8], text: &str) {
+    let offset = range.as_ptr() as usize - mmb.file.as_ptr() as usize;
+
+    println!("{:16x} .. {:16x} {}", offset, offset + range.len(), text);
+}
+
+fn handle(path: &Path, mmb: Mmb) -> Result<()> {
+    println!(
+        "{}: mmb version {} ({} bytes)\n",
+        path.display(),
+        mmb.version,
+        mmb.file.len()
+    );
+
+    println!("File header:");
+    println!("number of sorts: {}", mmb.num_sorts);
+    println!("number of terms and definitions: {}", mmb.num_terms);
+    println!("number of theorems and axioms: {}", mmb.num_theorems);
+
+    println!("");
+    println!("           start ..              end");
+
+    print_offsets(&mmb, mmb.sorts, "sort table");
+    print_offsets(&mmb, mmb.terms, "term table");
+    print_offsets(&mmb, mmb.theorems, "theorem table");
+    print_offsets(&mmb, mmb.proofs, "proof section");
+    print_offsets(&mmb, mmb.sort_index, "sort index");
+    print_offsets(&mmb, mmb.term_index, "term index");
+    print_offsets(&mmb, mmb.theorem_index, "theorem index");
 
     Ok(())
 }
